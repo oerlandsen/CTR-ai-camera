f49c318df2f7adf67c9537f6023ee9f30f561bbc
oerlandsen <oerlandsen@uc.cl>
2024-05-31 15:17:17 -0400

    fix: typo

--- project/camera_records.py
+++ project/camera_records.py
@@ -1,35 +1,29 @@
 
 import logging
-import threading
 
 logging.basicConfig(level=logging.DEBUG)
 
 class CameraRecords:
-    _lock = threading.Lock()
     _instance = None
 
     def __new__(cls, *args, **kwargs):
-        with cls._lock:
-            if not cls._instance:
-                cls._instance = super(CameraRecords, cls).__new__(cls, *args, **kwargs)
-                logging.debug("Creating a new instance of CameraRecords")
+        if not cls._instance:
+            cls._instance = super(CameraRecords, cls).__new__(cls, *args, **kwargs)
+            logging.debug("Creating a new instance of CameraRecords")
         return cls._instance
 
     def __init__(self):
         if not hasattr(self, 'initialized'):
-        
             self.value = 0
             self.initialized = True
             logging.debug("Initializing CameraRecords with value = 0")
 
     def store_result(self, result):
-        
         logging.debug(f"Storing result: {result} (current current value: {self.value})")
         self.value = result
         logging.debug(f"New value: {self.value}")
 
     def get_value(self):
-        
         logging.debug(f"Getting current value: {self.value}")
         return self.value
 
