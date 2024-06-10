65844e120d7f32a5a32ac0876496f236a96ae95e
oerlandsen <oerlandsen@uc.cl>
2024-06-10 11:26:07 -0400

    refactor for organize docker

--- project/consumer/workers_broker_app.py
+++ project/consumer/workers_broker_app.py
@@ -3,6 +3,14 @@ from celery import Celery
 from camera_records import CameraRecords
 from telegram_manager import TelegramManager
 from dotenv import load_dotenv
+import cv2
+import numpy as np
+import multiprocessing
+
+if __name__ == '__main__':
+    multiprocessing.set_start_method('spawn')
+
+
 
 load_dotenv()
 
@@ -29,10 +37,12 @@ CAMERAS = {
     }
 }
 
-# example of a task
-@celery_app.task
-def process_frame(frame_id, frame, camera_id):
-    print(f"Processing frame {type(frame)}")
+@celery_app.task(name="workers_broker_app.process_frame_task")
+def process_frame(data):
+    camera_id = data.get("camera_id")
+    frame_id = data.get("frame_id")
+    frame_content = data.get("frame_content")
+    frame = cv2.imdecode(np.frombuffer(frame_content, np.uint8), cv2.IMREAD_COLOR).tolist()
     camera_record = CAMERAS[camera_id]["camera_record"]
     telegram_manager = CAMERAS[camera_id]["telegram_manager"]
 
