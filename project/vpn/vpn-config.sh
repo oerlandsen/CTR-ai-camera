cd88f5a846bc002a60a9c8dfd3dc1706abd592ed
oerlandsen <oerlandsen@uc.cl>
2024-06-25 16:57:41 -0400

    feat: avances

--- project/vpn/vpn-config.sh
+++ project/vpn/vpn-config.sh
@@ -5,6 +5,7 @@ VPN_HOST="200.2.198.226"
 VPN_PORT="11443"
 VPN_USERNAME="ctr_cctv"
 VPN_PASSWORD="cctv.,ctr24"
+VPN_TRUSTED_CERT="42dc5002f823d9373d46aaf05c342f13fbb8e883d5ada48fc147b3bd322687e0"
 
 # Create openfortivpn configuration file
 cat <<EOF > /etc/openfortivpn/config
@@ -12,7 +13,7 @@ host = $VPN_HOST
 port = $VPN_PORT
 username = $VPN_USERNAME
 password = $VPN_PASSWORD
-trusted-cert = "sha256:your_cert_fingerprint_here"  # optional, based on your VPN server configuration
+trusted-cert = $VPN_TRUSTED_CERT
 EOF
 
 # Run openfortivpn
